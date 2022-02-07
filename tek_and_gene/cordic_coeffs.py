617f829badcca12d775be92c37162b43493e2b91
estimfr <95126843+estimfr@users.noreply.github.com>
2022-02-07 12:57:46 +0100

    Improve the Python cordic utility
    The cordic algo needs a final calculation to include the cumul of the 1/cos(h)
    This version has now a second simulation with more bits in the cumul

--- tek_and_gene/cordic_coeffs.py
+++ tek_and_gene/cordic_coeffs.py
@@ -4,12 +4,13 @@ for start_val in range(0, 2):
     a=range(start_val, 20)
     print("------------------------------------------------------------------------")
     print("Starting at " + str( start_val ))
+    print("Assume the input has been divided by 2, display of the error after some posttreatement")
     print("------------------------------------------------------------------------")
-    print("shifts\tarc tg\t\tangle\t\t\tnormalised 0..2.pi angle -> 0.00..1.00\t\t\tcumulative\tcumul")
+    print("shifts\tarc tg\t\tangle\t\t\tnormalised 0..2.pi angle -> 0.00..1.00\t\t\tcumulative\terror if 1.0+\terror if 1.0+")
     if start_val == 0:
-        title_c_p_h = "=0.5+0.625+0.03125+"
+        title_c_p_h = "0.125+0.625\t0.125+0.625+0.015625+0.0078125"
     else:
-        title_c_p_h = "=0.5+0.25+0.0625+"
+        title_c_p_h = "0.5+0.125\t0.5+0.125+0.0625+0.015625"
     print("N\t/\t\tradians\t\t\t8 bits\t16 bits\t\t24 bits\t\t32 bits\t\t1/cosine\t"+title_c_p_h)
     cumul_cos = 1.0
     list_16 = []
@@ -28,15 +29,18 @@ for start_val in range(0, 2):
         list_24.append(round(the_angle_24))
         list_32.append(round(the_angle_32))
         if start_val == 0:
-            cumul_pseudo_hex = "{:.12f}".format(cumul_cos - 0.5 - 0.0625 - 0.03125)
+            cumul_pseudo_hex   = "{:.12f}".format(2.0 * cumul_cos - 1.0 -0.125 - 0.0625)
+            cumul_pseudo_hex_2 = "{:.12f}".format(2.0 * cumul_cos - 1.0 -0.125 - 0.0625 - 0.015625 - 0.0078125 )
         else:
-            cumul_pseudo_hex = "{:.12f}".format(cumul_cos - 0.5 - 0.25 - 0.0625)
+            cumul_pseudo_hex   = "{:.12f}".format(2.0 * cumul_cos - 1.0 - 0.5 - 0.125)
+            cumul_pseudo_hex_2 = "{:.12f}".format(2.0 * cumul_cos - 1.0 - 0.5 - 0.125 - 0.0625 - 0.015625)
         print(str(ind) + ":\t" + "{:.10f}".format(the_shift) + "\t" + str(the_angle) + "\t",end='')
         print("{:7.4f}".format(the_angle_8) + "\t" + "{:8.3f}".format(the_angle_16) + "\t",end='')
         print("{:10.2f}".format(the_angle_24) + "\t" + "{:11.1f}".format(the_angle_32) + "\t",end='')
-        print("{:.12f}".format(1 / cumul_cos) + "\t" + cumul_pseudo_hex)
+        print("{:.12f}".format(1 / cumul_cos) + "\t" + cumul_pseudo_hex,end='')
+        print("\t" + cumul_pseudo_hex_2)
 
-    print("C plus plus list")
+    print("C plus plus list, ",end='')
     print("replace [] by {}")
     print(list_16)
     print(list_24)
